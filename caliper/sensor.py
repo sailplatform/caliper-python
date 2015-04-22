ac4cd21da9f8855cd4526938ccaefb0f27f3eda6
Viktor Haag <Viktor.Haag@D2L.com>
2015-04-22 14:10:41 -0400

    license fixup

--- caliper/sensor.py
+++ caliper/sensor.py
@@ -1,39 +1,22 @@
 # -*- coding: utf-8 -*-
 # Caliper-python package, sensor module
 #
-# Copyright (c) 2015 IMS Global Learning Consortium, Inc. All Rights Reserved.
-# Trademark Information- http://www.imsglobal.org/copyright.html
-
-# IMS Global Caliper Analytics™ APIs are publicly licensed as Open Source
-# Software via GNU General Public License version 3.0 GPL v3. This license
-# contains terms incompatible with use in closed-source software including a
-# copyleft provision.
-
-# IMS Global also makes available an Alternative License based on the GNU Lesser
-# General Public License. LGPL v3 Licensees (via the Alternative License) are
-# required to be IMS Global members. Membership in IMS is a commitment by a
-# supplier to the IMS community for ongoing support for achieving "plug and play"
-# integration.  IMS Membership dues pay for ongoing maintenance for the
-# Alternative License to be applicable to updates to the Caliper Analytics
-# APIs. The rationale for this dual-license approach and membership component is
-# to help assure a requisite level of ongoing development, project management,
-# and support for the software.
-
-# Licensees of IMS Global Caliper Analytics APIs are strongly encouraged to
-# become active contributors to the Caliper Analytics project and other projects
-# within IMS Global. Prospective licensees should understand that their initial
-# base contribution and ongoing membership fees are insufficient to fully fund
-# the ongoing development and maintenance of Caliper APIs and that voluntary
-# contributions are the primary "fuel" ensuring any open source project's
-# viability. Contributions can include development, bug fixing, bug reporting,
-# performance analysis, and other aspects of the overall development process.
-
-# Contributor status at the "github" level will be individual-based. Contributors
-# will need to sign an IMS Global Contributor License Agreement (CLA) that grants
-# IMS Global a license to contributions.
-
-# If you are interested in licensing the IMS Global Caliper Analytics APIs please
-# email licenses@imsglobal.org
+# This file is part of the IMS Caliper Analytics(tm) and is licensed to IMS
+# Global Learning Consortium, Inc. (http://www.imsglobal.org) under one or more
+# contributor license agreements.
+#
+# IMS Caliper is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
 
 from __future__ import absolute_import
 
