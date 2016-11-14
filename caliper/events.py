2e27678a8cb69011ab0676b6777593660279868b
Viktor Haag <viktor.haag@gmail.com>
2016-11-14 10:53:03 -0500

    switch federatedSession to check for type based on URI

--- caliper/events.py
+++ caliper/events.py
@@ -75,7 +75,7 @@ class Event(BaseEvent):
         self._set_date_prop('eventTime', eventTime, req=True)
         self._set_dict_prop('extensions', extensions)
         self._set_obj_prop('object', event_object, t=BaseEntity)
-        self._set_obj_prop('federatedSession', federatedSession, t=Session)
+        self._set_obj_prop('federatedSession', federatedSession, t=ENTITY_TYPES['SESSION'])
         self._set_obj_prop('generated', generated, t=entities.Generatable)
         self._set_obj_prop('group', group, t=ENTITY_TYPES['ORGANIZATION'])
         self._set_str_prop('id', id),
@@ -83,7 +83,7 @@ class Event(BaseEvent):
                            membership,
                            t=ENTITY_TYPES['MEMBERSHIP'])
         self._set_obj_prop('referrer', referrer, t=entities.Referrable)
-        self._set_obj_prop('session', session, t=Session)
+        self._set_obj_prop('session', session, t=ENTITY_TYPES['SESSION'])
         self._set_obj_prop('target', target, t=entities.Targetable)
 
     def as_minimal_event(self):
